e0e16878f6260669c72116a4b089163e5650f5f3
darkVanci <1585781755@qq.com>
2023-12-04 13:23:12 +0800

    添加了异常处理

--- pysql/exec.py
+++ pysql/exec.py
@@ -4,6 +4,13 @@ from .switch import get_stat
 
 def exec_command(command: str):
     stat = get_stat()
-    functions[stat][command]()
-    print(stat)
+    try:
+        functions[stat][command]()
+    except IndexError as e:
+        print(e)
+        print("发生了索引错误，请重新输入命令")
+    except:
+        print("发生了未知错误，请重新输入命令")
+    finally:
+        print('stat: ', stat)
 
